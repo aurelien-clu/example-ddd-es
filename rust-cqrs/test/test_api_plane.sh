4e6bf172c4bf657475cd3cbe246e61e8139d0355
Aurelien Somename <aurelien.somename@gmail.com>
2023-03-30 18:34:18 +0200

    wip

--- rust-cqrs/test/test_api_plane.sh
+++ rust-cqrs/test/test_api_plane.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
 RANDOM=$$
-TEST_ACCT="test-acct-$RANDOM"
+TEST_ACCT="test-plane-$RANDOM"
 TEST_URL="localhost:3030/plane/$TEST_ACCT"
 
-echo "******************************"
-echo "Using test plane: $TEST_ACCT"
-echo "******************************"
+echo "***************************"
+echo "* Plane: $TEST_ACCT"
+echo "***************************"
 
-echo "Registering an plane"
-curl -i --location --request POST $TEST_URL --header 'Content-Type: application/json' --data-raw "{\"Register\": {\"registration_id\": \"$TEST_ACCT\"}}"
+echo "Registering plane"
+curl --location --request POST $TEST_URL --header 'Content-Type: application/json' --data-raw "{\"Register\": {\"registration_id\": \"$TEST_ACCT\"}}"
 
-echo "\nUpdating position"
-curl -i --location --request POST $TEST_URL --header 'Content-Type: application/json' --data-raw "{\"UpdatePosition\": {\"latitude\": 1.0, \"longitude\": 2.0, \"altitude\": 0 }}"
+echo -e "\nUpdating position"
+curl --location --request POST $TEST_URL --header 'Content-Type: application/json' --data-raw "{\"UpdatePosition\": {\"latitude\": 1.0, \"longitude\": 2.0, \"altitude\": 0 }}"
 
-echo "\nPrepare for take off!"
-curl -i --location --request POST $TEST_URL --header 'Content-Type: application/json' --data-raw "{\"TakeOff\": null}"
+echo -e "\nPrepare for take off!"
+curl --location --request POST $TEST_URL --header 'Content-Type: application/json' --data-raw "{\"TakeOff\": null}"
 
-echo "\nUpdating position"
-curl -i --location --request POST $TEST_URL --header 'Content-Type: application/json' --data-raw "{\"UpdatePosition\": {\"latitude\": 10.0, \"longitude\": 20.0, \"altitude\": 10 }}"
+echo -e "\nUpdating position"
+curl --location --request POST $TEST_URL --header 'Content-Type: application/json' --data-raw "{\"UpdatePosition\": {\"latitude\": 10.0, \"longitude\": 20.0, \"altitude\": 10 }}"
 
-echo "\nJourney"
-curl -i --location $TEST_URL
+echo -e "\nJourney"
+curl --location $TEST_URL
 
-echo "\nUpdating position"
-curl -i --location --request POST $TEST_URL --header 'Content-Type: application/json' --data-raw "{\"UpdatePosition\": {\"latitude\": 11.0, \"longitude\": 21.0, \"altitude\": 20 }}"
+echo -e "\n\nUpdating position"
+curl --location --request POST $TEST_URL --header 'Content-Type: application/json' --data-raw "{\"UpdatePosition\": {\"latitude\": 11.0, \"longitude\": 21.0, \"altitude\": 20 }}"
 
-echo "\nPrepare for usual landing!"
-curl -i --location --request POST $TEST_URL --header 'Content-Type: application/json' --data-raw "{\"Land\": null}"
+echo -e "\nPrepare for usual landing!"
+curl --location --request POST $TEST_URL --header 'Content-Type: application/json' --data-raw "{\"Land\": null}"
 
-echo "\nJourney"
-curl -i --location $TEST_URL
+echo -e "\nJourney"
+curl --location $TEST_URL
+echo -e "\n"
